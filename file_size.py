68bfd13ed4a237812ad4367f41507e40f4c13943
toprogramm <toprogramm@gmail.com>
2024-05-21 04:11:35 +0600

    update

--- file_size.py
+++ file_size.py
@@ -1,16 +1,21 @@
 import os
 
-def list_files_larger_than_1gb(directory):
+def list_files_larger_than_min_size(directory, min_size_gb):
+    count = 0
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             file_size_gb = os.path.getsize(file_path) / (1024 * 1024 * 1024) # Convert bytes to GB
-            if file_size_gb > 1:
-                print(f"File: {file_path} ({file_size_gb:.2f} GB)")
+            if file_size_gb > min_size_gb:
+                count += 1
+                print(f"{count}. File: {file_path} ({file_size_gb:.2f} GB)")
 
-# Replace 'directory' with the path to your folder
-directory = r"E:\vid\learn\path"
-list_files_larger_than_1gb(directory)
+def main():
+    directory = input("Enter the directory path: ")
+    min_size_gb = float(input("Enter the minimum file size (in GB): "))
+    print(f"Files larger than {min_size_gb} GB in directory '{directory}':")
+    list_files_larger_than_min_size(directory, min_size_gb)
 
-
+if __name__ == "__main__":
+    main()
 
--- frame_counter.py
+++ frame_counter.py
@@ -1,10 +1,17 @@
 import ffmpeg
+import os
 
 def get_frame_count(video_path):
     probe = ffmpeg.probe(video_path)
     video_info = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
     return int(video_info['nb_frames'])
 
-video_path = "C:/path/to/file/file.mp4"
-frame_count = get_frame_count(video_path)
-print("Количество кадров в видео:", frame_count)
+def main():
+    video_directory = input("Enter the directory path of the video file: ")
+    video_name = input("Enter the name of the video file (including extension): ")
+    video_path = os.path.join(video_directory, video_name)
+    frame_count = get_frame_count(video_path)
+    print("Number of frames in the video:", frame_count)
+
+if __name__ == "__main__":
+    main()
