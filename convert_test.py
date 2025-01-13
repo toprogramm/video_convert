cb5efd8f1fbc7ed7a70d6b2369db239e1d45df62
toprogramm <toprogramm@gmail.com>
2025-01-14 00:56:23 +0600

    added experimental fps

--- convert_test.py
+++ convert_test.py
@@ -18,20 +18,19 @@ def check_ffmpeg_installed():
 
 def install_ffmpeg():
     if os.name == "nt":  # Windows
-        print("FFmpeg not found. Installing FFmpeg...")
-        os.system("Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))")
-        os.system("choco upgrade ffmpeg")
+        print("FFmpeg не найден. Установка FFmpeg...")
+        os.system("choco install ffmpeg")
     else:  # Linux / MacOS
-        print("FFmpeg not found. Installing FFmpeg...")
+        print("FFmpeg не найден. Установка FFmpeg...")
         os.system("sudo apt-get update && sudo apt-get install -y ffmpeg")
 
 def format_time(seconds):
     mins, secs = divmod(seconds, 60)
     hours, mins = divmod(mins, 60)
-    return f"{int(hours)}h:{int(mins)}m:{int(secs)}s"
+    return f"{int(hours)}ч:{int(mins)}м:{int(secs)}с"
 
-def apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate):
-    total_start_time = time.time()  # Start time of the script execution
+def apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate, fps):
+    total_start_time = time.time()  # Время начала выполнения скрипта
     
     if gpu_vendor in ['n', 'nvidia']:
         if codec.lower() == 'h264':
@@ -39,7 +38,7 @@ def apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate):
         elif codec.lower() == 'h265':
             codec_name = 'hevc_nvenc'
         else:
-            print("Unsupported codec.")
+            print("Неподдерживаемый кодек.")
             return
     elif gpu_vendor in ['a', 'amd']:
         if codec.lower() == 'h264':
@@ -47,10 +46,10 @@ def apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate):
         elif codec.lower() == 'h265':
             codec_name = 'hevc_amf'
         else:
-            print("Unsupported codec.")
+            print("Неподдерживаемый кодек.")
             return
     else:
-        print("Unsupported GPU vendor.")
+        print("Неподдерживаемый производитель видеокарты.")
         return
 
     for root, dirs, files in os.walk(directory_path):
@@ -58,48 +57,60 @@ def apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate):
             if file.endswith('.mp4'):
                 input_file = os.path.join(root, file)
                 output_file = os.path.join(root, '.'.join(file.split('.')[:-1]) + f'_mod.mp4')
-                command = f'ffmpeg -i "{input_file}" -c:v {codec_name} -b:v {bitrate}k -c:a copy "{output_file}"'
+                command = (
+                    f'ffmpeg -i "{input_file}" -r {fps} -c:v {codec_name} -b:v {bitrate}k -c:a copy "{output_file}"'
+                )
                 
-                start_time = time.time()  # Start time of file encoding
+                start_time = time.time()  # Время начала кодирования файла
                 os.system(command)
-                end_time = time.time()  # End time of file encoding
+                end_time = time.time()  # Время окончания кодирования файла
                 
                 input_size = os.path.getsize(input_file)
                 output_size = os.path.getsize(output_file)
                 
                 file_time = end_time - start_time
-                print(f"{Fore.RED}Input file: {input_file}, Size: {int(input_size / 1048576)} MB{Style.RESET_ALL}")
-                print(f"{Fore.RED}Output file: {output_file}, Size: {int(output_size / 1048576)} MB{Style.RESET_ALL}")
-                print(f"{Fore.YELLOW}File encoding time: {file_time:.2f} seconds ({format_time(file_time)}){Style.RESET_ALL}")
+                print(f"{Fore.RED}Исходный файл: {input_file}, Размер: {int(input_size / 1048576)} мбайт{Style.RESET_ALL}")
+                print(f"{Fore.RED}Конечный файл: {output_file}, Размер: {int(output_size / 1048576)} мбайт{Style.RESET_ALL}")
+                print(f"{Fore.YELLOW}Время кодирования файла: {file_time:.2f} секунд ({format_time(file_time)}){Style.RESET_ALL}")
                 
-                os.remove(input_file)  # Removing the input file after conversion
-                # Renaming the output file without the "_mod" suffix
+                os.remove(input_file)  # Удаление исходного файла после конвертации
+                # Переименование выходного файла без суффикса "_mod"
                 new_output_file = os.path.join(root, '.'.join(file.split('.')[:-1]) + f'.mp4')
                 os.rename(output_file, new_output_file)
     
-    total_end_time = time.time()  # End time of the script execution
+    total_end_time = time.time()  # Время окончания выполнения скрипта
     total_time = total_end_time - total_start_time
-    print(f"{Fore.GREEN}Total script execution time: {total_time:.2f} seconds ({format_time(total_time)}){Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Общее время выполнения скрипта: {total_time:.2f} секунд ({format_time(total_time)}){Style.RESET_ALL}")
 
 if not check_ffmpeg_installed():
     install_ffmpeg()
 
-directory_path = input("Enter the directory path: ")
+directory_path = input("Введите путь к директории: ")
 
 while True:
-    gpu_vendor = input("Enter the GPU vendor (n - NVIDIA, a - AMD): ").lower()
+    gpu_vendor = input("Введите производителя видеокарты (n - NVIDIA, a - AMD): ").lower()
     if gpu_vendor in ['n', 'nvidia', 'a', 'amd']:
         break
     else:
-        print("Incorrect value entered. Please enter 'n' for NVIDIA or 'a' for AMD.")
+        print("Введено неверное значение. Пожалуйста, введите 'n' для NVIDIA или 'a' для AMD.")
 
 while True:
-    codec = input("Enter the codec (h264 or h265): ").lower()
+    codec = input("Введите кодек (h264 или h265): ").lower()
     if codec in ['h264', 'h265']:
         break
     else:
-        print("Incorrect value entered. Please enter 'h264' or 'h265'.")
+        print("Введено неверное значение. Пожалуйста, введите 'h264' или 'h265'.")
 
-bitrate = input("Enter the bitrate (in kilobits/second): ")
+bitrate = input("Введите битрейт (в килобитах/сек): ")
 
-apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate)
+while True:
+    try:
+        fps = int(input("Введите желаемую частоту кадров (FPS): "))
+        if fps > 0:
+            break
+        else:
+            print("FPS должно быть положительным числом.")
+    except ValueError:
+        print("Пожалуйста, введите корректное числовое значение для FPS.")
+
+apply_ffmpeg_command(directory_path, gpu_vendor, codec, bitrate, fps)
